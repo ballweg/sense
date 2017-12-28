dbee2f3191d6755bcb942b7d7e072735c0f5422f
Jeff Ballweg <ballweg@users.noreply.github.com>
2017-12-28 15:16:33 -0600

    get samples, build object

--- sense.js
+++ sense.js
@@ -100,3 +100,70 @@ function sense(){
       .attr("d", line1);
   });
 }
+
+
+function getSample(token){
+  var sample = [];
+  // get Humidity
+  var date = new Date();
+  var dateStr = date.getDate() + "-"+ (date.getMonth() + 1) + "-"+ date.getFullYear() +"-"+date.getHours() + ":" + date.getMinutes();
+  console.log(dateStr);
+  // humidity =
+  get('https://us.wio.seeed.io/v1/node/GroveTempHumD0/humidity?access_token='+token)
+    .then(function(response, sample) {
+      Data.push(response);
+      d = JSON.parse(response);
+      d.date = dateStr;
+      console.log("Success (hum)!", d);
+      return d;
+    }, function(error) {
+      console.error("Failed (hum)!", error);
+    }).then(function(sample){
+      console.log("sample before temp call: ", sample);
+      get('https://us.wio.seeed.io/v1/node/GroveTempHumD0/temperature?access_token='+token)
+        .then(function(response, sample) {
+          // Data.push(response);
+          temp = JSON.parse(response);
+          // sample.date = dateStr;
+          console.log("Success (temp)!", temp);
+          return temp;
+        }, function(error) {
+          console.error("Failed (temp)!", error);
+        }).then(function(temp){
+          console.log("temp after temp call: ", temp);
+          sample.temp = temp.celsius_degree;
+          console.log("sample after temp call:", sample);
+        });
+    });
+}
+
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
